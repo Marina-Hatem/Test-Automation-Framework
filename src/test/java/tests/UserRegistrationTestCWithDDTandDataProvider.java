a1dff80d6bb18cb85324cf09a80d25ae04e67516
marinaHatem <marina.hatem@outlook.com>
2022-05-29 21:48:27 +0200

    Added UserRegisterSuccessfully test case by DDT with data provider annotation

--- src/test/java/tests/UserRegistrationTestCWithDDTandDataProvider.java
+++ src/test/java/tests/UserRegistrationTestCWithDDTandDataProvider.java
@@ -1,4 +1,5 @@
 package tests;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -7,6 +8,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,7 +18,7 @@ import pages.UserRegistrationPage;
 import utilities.Helper;
 import utilities.WebDrivers;
 
-public class UserRegistrationTestC 
+public class UserRegistrationTestCWithDDTandDataProvider 
 {
 	WebDriver driver;
 	WebDrivers d = new WebDrivers();
@@ -24,11 +26,6 @@ public class UserRegistrationTestC
 	HomePage homeP ;
 	UserRegistrationPage UReg ;
 	LoginPage LoginP;
-	
-	String Fname ="Marina";
-	String Lname ="Hatem";
-	String Email = "Test1@gmail.com";
-	String OldPassword ="1234GO";
 	
 	@Parameters({"browser"})
 	@BeforeTest
@@ -41,36 +38,57 @@ public class UserRegistrationTestC
 		driver.manage().window().fullscreen();
 	}
 
-	@Test(priority = 1)
-	void UserCanRegisterSuccessFully() throws InterruptedException
+	
+	
+	//Implementing DDT by Providing data to the tests
+	@DataProvider(name = "RegistrationTestData")
+	public static Object[][] userData()
+	{
+		return new Object[][]
+				{
+			{
+				"Marina", "Hatem", "mmh1@gmail.com", "1234GO"
+			},
+			{
+				"Habib", "Hanna", "hh1@gmail.com", "1234567"
+			},
+			{
+				"Mona", "Hamdy", "mh2@gmail.com", "1234567"
+			},
+			{
+				"Enas", "Fouad", "ef@gmail.com", "1234567"
+			}
+				};
+	}
+	
+	
+	@Test(priority = 1, dataProvider = "RegistrationTestData")
+	void UserCanRegisterSuccessFully(String fname, String Lname, String email, String pwd)
 	{
 		UReg = new UserRegistrationPage(driver);
 		homeP = new HomePage(driver);
+		LoginP = new LoginPage(driver);
 		
+		//Register
 		homeP.OpenRegistrationPage();
-		UReg.Register(Fname, Lname, Email , OldPassword);
-		
+		UReg.Register(fname, Lname, email , pwd);
 		//asserting on success Message
 		Assert.assertEquals("Your registration completed", UReg.sucssesMSG.getText());
-	}
-	
-	@Test(dependsOnMethods = {"UserCanRegisterSuccessFully"})
-	void RegisteredUserCanLogOut()
-	{
+		
+		
+		//Log Out
 		UReg.Logout();
-	}
+			
 
-	
-	@Test(dependsOnMethods = {"RegisteredUserCanLogOut"})
-	void RegisteredUserCanLogin()
-	{
+		//Login
 		homeP.OpenLoginPage();
-		LoginP = new LoginPage(driver);
-		LoginP.Login(Email , OldPassword);
+		LoginP.Login(email , pwd);
 		//asserting that log out button is displayed after login
 		Assert.assertTrue(UReg.LogOutBtn.isDisplayed());
+		
+		//Log Out again to rerun the test
+		UReg.Logout();
 	}
-
 
 	@AfterTest
 	void EndDriver()
